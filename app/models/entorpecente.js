836a902abf97ece57142d85ff63cf04423abad62
Ícaro Forte <icaro_forte@hotmail.com>
2019-06-27 11:55:45 -0300

    Fixing layout bugs

--- app/models/entorpecente.js
+++ app/models/entorpecente.js
@@ -1,6 +1,36 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+/*
+    Modelo de classe entorpecente para o sistema.
+    
+    # API
+    
+    ```
+
+    @class Entorpecentes.
+    @param userName?: string - O userName de quem cadastrou o Entorpecente.
+	@param procedimento?: string - O tipo de procedimento.
+	@param n_proced?: string - O código de procedimento.
+	@param data_entrada: Date - Data de entrada do procedimento.
+	@param investiado?: string - O nome do suspeito de portar a substância apreendida.
+	@param substancia?: string - Qual a substância foi apreendida.
+	@param quant?: string - A quantidade que foi apreendida.
+	@param und?: string - O sistema de unidade de medida utilizado.
+	@param laudo?: string - O código do laudo do entorpecente.
+	@param vara?: string - A vara que foi enviado o laudo.
+	@param n_processo?: string - O código do processo.
+	@param lacre?: string - O código do lacre.
+	@param data_autorizacao?: Date - Data de autorização.
+	@param caixa?: string - A caixa onde a droga está armazenada.
+	@param oficio?: string - O ofício emitido para o entorpecente.
+	@param delegacia?: string - De qual delegacia veio o entorpecente.
+	
+	```
+
+*/
+
+
 var EntorpecenteSchema = new Schema({
   username:{
   		type: String,
